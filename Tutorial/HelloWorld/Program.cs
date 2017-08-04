c1fd3d83471c1f5ee8010cd3cba4821230d413e9
Van Pham <van.pham@gruden.com>
2017-08-05 01:33:48 +0700

    Initial commit

---
+++ Tutorial/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = "Hello world.";
+            
+            Console.WriteLine(message);
+
+            Console.ReadLine();
+        }
+    }
+}
