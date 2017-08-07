ed1d3355a0ad10d685efca08512ee600cee86190
luongtran <luongtran1717@gmail.com>
2017-08-07 15:39:47 +0700

    Bài 1

---
+++ Tutorial/sourcecode/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static int sum;
+        public static int Sum
+        {
+            get { return sum; }
+            set { sum = value; }
+        }
+
+        static void Main(string[] args)
+        {
+            Program.Sum = 0;
+
+            for (int i = 0; i < 10000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    Program.Sum = Program.Sum + i;
+                }
+            }
+
+            Console.WriteLine("Sum of all the multiples of 3 or 5 below 1000 is: " + Program.Sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tutorial/sourcecode/Employee/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Employee
+{
+    class Employee
+    {
+        
+    }
+}
