11dedaf8b26102c864f9ea898a0a3a0dead8addf
Van Pham <van.pham@gruden.com>
2017-08-07 18:31:16 +0700

    Solve problem 1

---
+++ Tutorial/sourcecode/Practice.App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Practice.Core;
+
+namespace Practice.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var example = new MultiplesOf3And5();
+            var result = example.Sum(1000);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
