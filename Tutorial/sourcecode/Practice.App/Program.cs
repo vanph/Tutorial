ffc82b7de33fab3de6375e131e87575b109ff8c2
luongtran <luongtran1717@gmail.com>
2017-08-09 15:29:18 +0700

    HomeWork_commit lại

--- Tutorial/sourcecode/Practice.App/Program.cs
+++ Tutorial/sourcecode/Practice.App/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-<<<<<<< HEAD
+
 using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
 using Practice.App.Lesson0;
 using Practice.Problems;
-=======
 using Practice.Problems.Lesson0;
->>>>>>> homework_lesson0
+
 
 namespace Practice.App
 {
@@ -15,32 +14,6 @@ namespace Practice.App
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            //var example = new MultiplesOf3And5();
-            //Console.WriteLine(example.Sum(1000));
-
-            var employees = new List<Employee>();
-
-            if (File.Exists("App_Data/Employee.txt"))
-            {
-                var lines = FileHelper.ReadFile("App_Data/Employee.txt");
-                if (lines != null && lines.Length > 0)
-                {
-                    foreach (var line in lines)
-                    {
-                        //var data = line.Split('|');
-                        var data = Regex.Split(line, @"\|");
-                        var emp = new Employee() { FirstName = data[0] };
-
-                        employees.Add(emp);
-                    }
-                }
-            }
-
-            foreach (var emp in employees)
-            {
-                Console.WriteLine(emp.FirstName);
-=======
             DisplayEmployee("Employee.txt");
         }
 
@@ -51,7 +24,7 @@ namespace Practice.App
             foreach( var line in employees)
             {
                 Console.WriteLine(line.EmailAddress);
->>>>>>> homework_lesson0
+
             }
             Console.ReadLine();
 
