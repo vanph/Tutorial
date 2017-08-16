d8c79a5daca41436480fd7d1a2f240dcb02817da
Van Pham <van.pham@gruden.com>
2017-08-16 16:37:59 +0700

    Add MyCountry project

---
+++ Tutorial/sourcecode/MyCountry/Model/City.cs
@@ -0,0 +1,16 @@
+﻿namespace MyCountry.Model
+{
+    public  class City
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        public City(string code, string name, string type)
+        {
+            Code = code;
+            Name = name;
+            Type = type;
+        }
+    }
+}
---
+++ Tutorial/sourcecode/MyCountry/Model/District.cs
@@ -0,0 +1,18 @@
+﻿namespace MyCountry.Model
+{
+   public class District
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string CityCode { get; set; }
+
+        public District(string code, string name, string type, string cityCode)
+        {
+            Code = code;
+            Name = name;
+            Type = type;
+            CityCode = cityCode;
+        }
+    }
+}
