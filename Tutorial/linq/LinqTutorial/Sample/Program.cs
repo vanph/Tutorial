ba65a13de33e4e759e319601ec2599c49061c46d
luongtran1717 <luongtran1717@gmail.com>
2017-09-04 03:38:10 -0700

    Luong test comit git

--- Tutorial/linq/LinqTutorial/Sample/Program.cs
+++ Tutorial/linq/LinqTutorial/Sample/Program.cs
@@ -12,7 +12,7 @@ namespace Sample
             var person = new Person {FirstName = "Luong", MiddleName = "Van",SurName = "Tran"};
             
             Console.WriteLine(person.FullName);
-            //Console.WriteLine(p.SayHello("Welcome"));
+            Console.WriteLine(person.SayHello("Welcome"));
             //Console.WriteLine("LastName");
             //Console.WriteLine(nameof(p.SurName));
 
