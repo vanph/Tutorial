3d07687feb321775c89192ced30574ecedc9f6e3
vanph <vanphbk@gmail.com>
2017-09-11 22:06:29 +0700

    Add sample projects

---
+++ Tutorial/samples/Nullable Sample/Basics/Basics.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+namespace Basics
+{
+    internal class NullableBasics
+    {
+        static void DisplayValue(int? num)
+        {
+            if (num.HasValue == true)
+            {
+                Console.WriteLine("num = " + num);
+            }
+            else
+            {
+                Console.WriteLine("num = null");
+            }
+
+            // num.Value throws an InvalidOperationException if num.HasValue is false
+            try
+            {
+                Console.WriteLine("value = {0}", num.Value);
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void Main()
+        {
+            DisplayValue(1);
+            DisplayValue(null);
+        }
+    }
+}
